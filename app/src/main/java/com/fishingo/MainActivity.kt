4653aa0a91679ff8bd2af7e4979fc1028d546e0e
Serban Cosmin-Daniel <cosminserban2003@gmail.com>
2025-10-16 23:05:22 +0300

    Simple menu with buttons for testing the preview on mobile phone

--- app/src/main/java/com/fishingo/MainActivity.kt
+++ app/src/main/java/com/fishingo/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.fishingo.ui.theme.FishinGoTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +24,41 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            FishinGoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            MainMenu()
                 }
+            }
+        }
+
+@Composable
+fun MainMenu() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment=Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text="FishinGo",
+                fontSize = 35.sp,
+                style = MaterialTheme.typography.titleLarge
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(onClick = {}){
+                Text("Fishnet")
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Button(onClick={}){
+                Text("Go Fish")
             }
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    FishinGoTheme {
-        Greeting("Android")
-    }
+fun MainMenuPreview(){
+    MainMenu()
 }
