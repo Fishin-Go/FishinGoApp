875add90d3060d5eeb91e7b61370227a9ad871df
Serban Cosmin-Daniel <cosminserban2003@gmail.com>
2025-11-22 20:38:29 +0200

    db pass is now a system variable + gradle fix for deploying backend on railway

--- app/src/main/java/com/fishingo/network/ApiClient.kt
+++ app/src/main/java/com/fishingo/network/ApiClient.kt
@@ -3,6 +3,7 @@ package com.fishingo.network
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+
 object ApiClient {
 
     // IMPORTANT:
@@ -11,7 +12,7 @@ object ApiClient {
     // - If you're using a physical phone on the same Wi-Fi, replace this with your PC's LAN IP,
     //   e.g. "http://192.168.0.23:8080/"
 
-    private const val BASE_URL = "http://10.0.2.2:8080/"
+    private const val BASE_URL = "http://192.168.1.141:8080/"
 
     val authApi: AuthApi by lazy {
         Retrofit.Builder()
